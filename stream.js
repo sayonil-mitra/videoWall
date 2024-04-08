669aa28f4017fe09be78e8f89a5aff4e76339324
sayonil-mitra <139944160+sayonil-mitra@users.noreply.github.com>
2024-04-08 19:10:38 +0530

    made test code a usable function

--- stream.js
+++ stream.js
@@ -1,35 +1,39 @@
 import ZoomVideo from "@zoom/videosdk";
 
-// init client an stream
-var client = ZoomVideo.createClient();
-var stream;
+async function attachVideoStream() {
+  // init client an stream
+  var client = ZoomVideo.createClient();
+  var stream;
 
-// join existing zoom session
-await client.init("en-US", "Global", { patchJsMedia: true });
-await client.join(
-  "sessionName",
-  "VIDEO_SDK_JWT",
-  "userName",
-  "sessionPasscode"
-);
-
-// attach video stream
-stream = client.getMediaStream();
-let userVideo = await stream.attachVideo(
-  client.getCurrentUserInfo().userId,
-  RESOLUTION
-);
-document.querySelector("video-player-container").appendChild(userVideo);
-
-// screen share
-if (stream.isStartShareScreenWithVideoElement()) {
-  await stream.startShareScreen(
-    document.querySelector("#my-screen-share-content-video")
+  // join existing zoom session
+  await client.init("en-US", "Global", { patchJsMedia: true });
+  await client.join(
+    "sessionName",
+    "VIDEO_SDK_JWT",
+    "userName",
+    "sessionPasscode"
   );
-  // screen share successfully started and rendered
-} else {
-  await stream.startShareScreen(
-    document.querySelector("#my-screen-share-content-canvas")
+
+  // attach video stream
+  stream = client.getMediaStream();
+  let userVideo = await stream.attachVideo(
+    client.getCurrentUserInfo().userId,
+    RESOLUTION
   );
-  // screen share successfully started and rendered
+  document.querySelector("video-player-container").appendChild(userVideo);
+
+  // screen share
+  if (stream.isStartShareScreenWithVideoElement()) {
+    await stream.startShareScreen(
+      document.querySelector("#my-screen-share-content-video")
+    );
+    // screen share successfully started and rendered
+  } else {
+    await stream.startShareScreen(
+      document.querySelector("#my-screen-share-content-canvas")
+    );
+    // screen share successfully started and rendered
+  }
 }
+
+module.exports = attachVideoStream;
